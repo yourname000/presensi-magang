be3264bf6dcfba7bcfa7d54f659079da62397df7
Lutfi Riani <lutfiriani1510@gmail.com>
2025-09-30 14:29:44 +0700

    first commit

--- public/assets/js/all.js
+++ public/assets/js/all.js
@@ -1,93 +1,59 @@
-// tambah shift
 function addShift() {
     $('#no_data_vector').addClass('d-none');
     $('#submit_shift').removeClass('d-none');
-    
-    let num = $('#data_add_shift').children('.row').length; 
-    let html = '';
 
-    // ROW UTAMA per shift
-    html += '<div id="pane_add_' + num + '" class="row pb-4 border-bottom mb-4">'; 
+    let num = $('#data_add_shift').children('.shift-box').length;
 
-    // ==========================================================
-    // BARIS 1: KODE SHIFT (col-4) & NAMA SHIFT (col-8) -> TOTAL 12
-    // ==========================================================
-    
-    // Kode Shift (col-4)
-    html += '    <div class="col-md-4 col-sm-12">'; 
-    html += '      <div class="mb-3" id="req_kode_new_' + num + '">'; 
-    html += '        <label class="form-label required">Kode Shift</label>'; 
-    html += '        <input type="text" name="kode[' + num + ']" class="form-control" placeholder="Contoh : S" autocomplete="off" />';
-    html += '      </div>';
-    html += '    </div>';
-
-    // Nama Shift (col-8)
-    html += '    <div class="col-md-8 col-sm-12">'; 
-    html += '      <div class="mb-3" id="req_nama_new_' + num + '">';
-    html += '        <label class="form-label required">Nama Shift</label>';
-    html += '        <input type="text" name="nama[' + num + ']" class="form-control" placeholder="Contoh : Shift Pagi" autocomplete="off" />';
-    html += '      </div>';
-    html += '    </div>';
-    
-    // ==========================================================
-    // BARIS 2: JAM MASUK (col-3), JAM PULANG (col-3), LEMBUR (col-4), HAPUS (col-2) -> TOTAL 12
-    // ==========================================================
-
-    // Jam Masuk (col-3)
-    html += '    <div class="col-md-3 col-sm-12">';
-    html += '      <div class="mb-3" id="req_jam_masuk_new_' + num + '">';
-    html += '        <label class="form-label required">Jam Masuk</label>';
-    html += '        <input type="time" name="jam_masuk[' + num + ']" class="form-control" placeholder="--:--" autocomplete="off" />';
-    html += '      </div>';
-    html += '    </div>';
-
-    // Jam Pulang (col-3)
-    html += '    <div class="col-md-3 col-sm-12">';
-    html += '      <div class="mb-3" id="req_jam_pulang_new_' + num + '">';
-    html += '        <label class="form-label required">Jam Pulang</label>';
-    html += '        <input type="time" name="jam_pulang[' + num + ']" class="form-control" placeholder="--:--" autocomplete="off" />';
-    html += '      </div>';
-    html += '    </div>';
-
-    // Lembur (col-4)
-    html += '    <div class="col-md-4 col-sm-10">';
-    html += '      <div class="mb-3" id="req_lembur_new_' + num + '">';
-    html += '        <label class="form-label">Batas Lembur (Menit)</label>';
-    html += '        <input type="number" name="lembur[' + num + ']" class="form-control" placeholder="-" autocomplete="off" />';
-    html += '      </div>';
-    html += '    </div>';
-
-    // Tombol Hapus (col-2)
-    html += '    <div class="col-md-2 col-sm-2 d-flex justify-content-start align-self-end">'; 
-    html += '      <button type="button" onclick="removeShift(this)" class="btn btn-danger btn-sm" title="Hapus" style="height: 38px; margin-bottom: 12px;">'; 
-    html += '        <i class="fa fa-trash"></i>'; 
-    html += '      </button>';
-    html += '    </div>';
-
-    html += '  </div>'; // Penutup row (pane_add)
+    let html = `
+    <div id="pane_add_${num}" class="shift-box p-4 mb-4 bg-light rounded-4 shadow-sm">
+        <div class="row">
+            <div class="col-md-6 mb-3">
+                <label class="form-label required">Kode Shift</label>
+                <input type="text" name="kode[${num}]" class="form-control" placeholder="Contoh : O" />
+            </div>
+            <div class="col-md-6 mb-3 d-flex">
+                <div class="flex-grow-1">
+                    <label class="form-label required">Nama Shift</label>
+                    <input type="text" name="nama[${num}]" class="form-control" placeholder="Contoh : Office" />
+                </div>
+                <button type="button" onclick="removeShift(this)" class="btn btn-danger ms-2 align-self-end">
+                    <i class="fa fa-trash"></i>
+                </button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 mb-3">
+                <label class="form-label required">Jam Masuk</label>
+                <input type="time" name="jam_masuk[${num}]" class="form-control" placeholder="--:--" />
+            </div>
+            <div class="col-md-4 mb-3">
+                <label class="form-label required">Jam Pulang</label>
+                <input type="time" name="jam_pulang[${num}]" class="form-control" placeholder="--:--" />
+            </div>
+            <div class="col-md-4 mb-3">
+                <label class="form-label">Batas Lembur (Menit)</label>
+                <input type="number" name="lembur[${num}]" class="form-control" placeholder="-" />
+            </div>
+        </div>
+    </div>`;
 
     $('#data_add_shift').append(html);
 }
 
-// Fungsi removeShift tetap sama
 function removeShift(el) {
-    // ... (kode re-indexing dan hapus shift)
-    // ... (pastikan semua indexing name dan id disesuaikan)
-    $(el).closest('.row').remove();
+    $(el).closest('.shift-box').remove();
 
-    // Re-indexing
-    $('#data_add_shift .row').each(function (i) {
+    // Re-index
+    $('#data_add_shift .shift-box').each(function (i) {
         $(this).attr('id', 'pane_add_' + i);
-
         $(this).find('input[name^="kode"]').attr('name', 'kode[' + i + ']');
         $(this).find('input[name^="nama"]').attr('name', 'nama[' + i + ']');
-
         $(this).find('input[name^="jam_masuk"]').attr('name', 'jam_masuk[' + i + ']');
         $(this).find('input[name^="jam_pulang"]').attr('name', 'jam_pulang[' + i + ']');
         $(this).find('input[name^="lembur"]').attr('name', 'lembur[' + i + ']');
     });
 
-    if ($('#data_add_shift .row').length === 0 && $('#data_shift .row').length === 0) {
+    if ($('#data_add_shift .shift-box').length === 0 && $('#data_shift .p-4').length === 0) {
         $('#no_data_vector').removeClass('d-none');
         $('#submit_shift').addClass('d-none');
     }
--- resources/views/pengaturan/page/shift.blade.php
+++ resources/views/pengaturan/page/shift.blade.php
@@ -1,82 +1,69 @@
-{{-- HAPUS TAG <script> DARI ATAS INI --}}
-
-{{-- Tambahkan pembungkus besar untuk meniru area konten utama --}}
 <div class="card p-5 mt-5"> 
-    
     <form id="form_shift" class="form" action="{{ route('setup.shift') }}" method="POST" enctype="multipart/form-data">
         @csrf 
         
+        {{-- Kondisi kosong --}}
         <div id="no_data_vector" class="w-100 d-flex {{ ($shift->isNotEmpty()) ? 'd-none' : '' }} justify-content-center align-items-center flex-column py-5">
-            {{-- Menggunakan kelas Bootstrap untuk padding --}}
-            <div class="background-partisi-contain" style="background-image : url('{{ image_check('empty.svg','default') }}');width : 300px;height : 250px;"></div>
+            <div class="background-partisi-contain" style="background-image:url('{{ image_check('empty.svg','default') }}');width:300px;height:250px;"></div>
             <h3 class="text-center text-info fs-3">Tidak Ada Data</h3>
             <p class="text-muted">Belum ada data shift! Silahkan tambahkan data shift terlebih dahulu</p>
         </div>
 
-        {{-- Daftar Shift yang Sudah Ada (Pastikan style di dalam loop tetap menggunakan col-md-X) --}}
-        <div id="data_shift" class="row px-lg-5 px-sm-0 {{ ($shift->isEmpty()) ? 'd-none' : '' }}">
-            @if($shift->isNotEmpty())
-                @foreach($shift AS $row)
-                <div class="col-12 card bg-white border p-3 mb-3">
-                    <div class="row">
-                        {{-- Baris 1 --}}
-                        <div class="col-md-4 col-sm-12">
-                            <div class="mb-3" id="req_kode_{{ $row->id_shift }}">
-                                <label class="form-label required">Kode Shift</label>
-                                <input type="text" name="edit_kode[{{ $row->id_shift }}]" class="form-control" value="{{ $row->kode }}" placeholder="Contoh : O" autocomplete="off" />
-                            </div>
+        {{-- Daftar Shift lama --}}
+        <div id="data_shift" class="{{ ($shift->isEmpty()) ? 'd-none' : '' }}">
+            @foreach($shift as $row)
+            <div class="p-4 mb-4 bg-light rounded-4 shadow-sm">
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <label class="form-label required">Kode Shift</label>
+                        <input type="text" name="edit_kode[{{ $row->id_shift }}]" class="form-control"
+                            value="{{ $row->kode }}" placeholder="Contoh : O" autocomplete="off" />
+                    </div>
+                    <div class="col-md-6 mb-3 d-flex">
+                        <div class="flex-grow-1">
+                            <label class="form-label required">Nama Shift</label>
+                            <input type="text" name="edit_nama[{{ $row->id_shift }}]" class="form-control"
+                                value="{{ $row->nama }}" placeholder="Contoh : Office" autocomplete="off" />
                         </div>
-                        <div class="col-md-7 col-sm-10"> 
-                            <div class="mb-3" id="req_nama_{{ $row->id_shift }}">
-                                <label class="form-label required">Nama Shift</label>
-                                <input type="text" name="edit_nama[{{ $row->id_shift }}]" class="form-control" value="{{ $row->nama }}" placeholder="Contoh : Office" autocomplete="off" />
-                            </div>
-                        </div>
-                        <div class="col-md-1 col-sm-2 d-flex align-items-center justify-content-center">
-                            <button type="button" data-reload="big" onclick="hapus_data(this,event,{{ $row->id_shift }},`shift`,`id_shift`)" class="btn btn-danger btn-sm" title="Delete" style="height: 38px; margin-top: 5px;"> 
-                                {{-- Style ditambahkan agar sejajar dengan input di sampingnya --}}
-                                <i class="fa fa-trash"></i> 
-                            </button>
-                        </div>
-                        {{-- Baris 2 --}}
-                        <div class="col-md-4 col-sm-12">
-                            <div class="mb-3" id="req_jam_masuk_{{ $row->id_shift }}">
-                                <label class="form-label required">Jam Masuk</label>
-                                <input type="time" name="edit_jam_masuk[{{ $row->id_shift }}]" class="form-control" value="{{ date('H:i',strtotime($row->jam_masuk)) }}" placeholder="Contoh : 07:00" autocomplete="off" />
-                            </div>
-                        </div>
-                        <div class="col-md-4 col-sm-12">
-                            <div class="mb-3" id="req_jam_pulang_{{ $row->id_shift }}">
-                                <label class="form-label required">Jam Pulang</label>
-                                <input type="time" name="edit_jam_pulang[{{ $row->id_shift }}]" class="form-control" value="{{ date('H:i',strtotime($row->jam_pulang)) }}" placeholder="Contoh : 17:00" autocomplete="off" />
-                            </div>
-                        </div>
-                        <div class="col-md-4 col-sm-12">
-                            <div class="mb-3" id="req_lembur_{{ $row->id_shift }}">
-                                <label class="form-label">Batas Lembur (Menit)</label>
-                                <input type="number" name="edit_lembur[{{ $row->id_shift }}]" class="form-control" value="{{ $row->lembur }}" placeholder="-" autocomplete="off" />
-                            </div>
-                        </div>
+                        <button type="button" onclick="hapus_data(this,event,{{ $row->id_shift }},'shift','id_shift')" 
+                                class="btn btn-danger ms-2 align-self-end">
+                            <i class="fa fa-trash"></i>
+                        </button>
                     </div>
                 </div>
-                @endforeach
-            @endif
+                <div class="row">
+                    <div class="col-md-4 mb-3">
+                        <label class="form-label required">Jam Masuk</label>
+                        <input type="time" name="edit_jam_masuk[{{ $row->id_shift }}]" class="form-control"
+                            value="{{ date('H:i',strtotime($row->jam_masuk)) }}" />
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label class="form-label required">Jam Pulang</label>
+                        <input type="time" name="edit_jam_pulang[{{ $row->id_shift }}]" class="form-control"
+                            value="{{ date('H:i',strtotime($row->jam_pulang)) }}" />
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label class="form-label">Batas Lembur (Menit)</label>
+                        <input type="number" name="edit_lembur[{{ $row->id_shift }}]" class="form-control"
+                            value="{{ $row->lembur }}" placeholder="-" />
+                    </div>
+                </div>
+            </div>
+            @endforeach
         </div>
 
-        {{-- Container untuk Shift Baru (yang akan diisi oleh addShift()) --}}
-        <div id="data_add_shift" class="row px-lg-5 px-sm-0">
-            {{-- Hasil dari addShift() akan masuk di sini --}}
-        </div>
+        {{-- Shift baru dari JS --}}
+        <div id="data_add_shift"></div>
 
-        {{-- Tombol Aksi --}}
-        <div class="w-100 d-flex justify-content-center align-items-center pt-5 pb-5"> 
-            <button type="button" id="plus_shift" onclick="addShift()" class="mx-5 btn-modal btn btn-info">
-                <i class="fa fa-plus"></i>
-                <span class="indicator-label">Tambah</span>
+        {{-- Tombol aksi --}}
+        <div class="w-100 d-flex justify-content-center align-items-center pt-4">
+            <button type="button" id="plus_shift" onclick="addShift()" class="btn btn-success me-3">
+                + Tambah
             </button>
-
-            <button type="button" id="submit_shift" data-loader="big" onclick="submit_form(this,'#form_shift')" class="mx-5 btn-modal btn btn-primary {{ ($shift->isEmpty()) ? 'd-none' : '' }}">
-                <span class="indicator-label">Simpan</span>
+            <button type="button" id="submit_shift" data-loader="big" 
+                onclick="submit_form(this,'#form_shift')" 
+                class="btn btn-warning {{ ($shift->isEmpty()) ? 'd-none' : '' }}">
+                Simpan
             </button>
         </div>
     </form>
